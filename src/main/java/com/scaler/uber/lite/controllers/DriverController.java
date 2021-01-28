377f987feeb54be9f78b37ab5b05483f37834469
Piyush Singh <piyush.singh@kronos.com>
2021-01-28 15:02:46 +0530

    Initial commit

--- src/main/java/com/scaler/uber/lite/controllers/DriverController.java
+++ src/main/java/com/scaler/uber/lite/controllers/DriverController.java
@@ -12,7 +12,14 @@ import com.scaler.uber.lite.repositories.ReviewRepository;
 import com.scaler.uber.lite.service.BookingService;
 import com.scaler.uber.lite.service.ServiceConstants;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
